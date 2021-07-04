bef6956f22327a398602d5f7c3dfd63326f82d84
kenkannih <84455320+kenkannih@users.noreply.github.com>
2021-07-04 19:25:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-    TOKEN = os.environ.get("TOKEN", "")
-    ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-    REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/kenkannih/Bokep-Bot")
-    OWNER = os.environ.get("OWNER", "")
-    BOT_NAME = os.environ.get("BOT_NAME", "")
+TOKEN = os.environ.get("TOKEN", "")
+ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
+REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/kenkannih/Bokep-Bot")
+OWNER = os.environ.get("OWNER", "")
+BOT_NAME = os.environ.get("BOT_NAME", "")
