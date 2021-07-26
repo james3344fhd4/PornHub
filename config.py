9faa497cff1f719443cd8c8d5971f8c62191af6a
levina <82658782+levina-lab@users.noreply.github.com>
2021-07-26 09:20:14 +0700

    setup

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 TOKEN = os.environ.get("TOKEN", "")
 ARQ_API_KEY = os.environ.get("ARQ_API_KEY", "")
-REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/kenkannih/Bokep-Bot")
+REPO_BOT = os.environ.get("REPO_BOT", "https://github.com/levina-lab/PornHub")
 OWNER = os.environ.get("OWNER", "")
 BOT_NAME = os.environ.get("BOT_NAME", "")
